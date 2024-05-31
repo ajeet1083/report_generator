9154cecc97601c6267841e936cd106ab547e56ab
ajeet@1083 <ajeetkumarmaurya67634@gmail.com>
2024-05-31 07:05:04 +0000

    Add build.sh script

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
+#!/bin/bash
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
+# Apply migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
